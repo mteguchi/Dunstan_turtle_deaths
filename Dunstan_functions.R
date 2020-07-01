df94a1454ec894be6a81f27975f0e265de7b3b12
Tomo Eguchi <tomoeguchi@pop-os.localdomain>
2020-07-01 15:55:22 -0700

    Adding poison observation model

--- Dunstan_functions.R
+++ Dunstan_functions.R
@@ -214,29 +214,38 @@ compute.summary.stats_St_K1_K2 <- function(samples, seasons, max.days, P){
   return(X_St_K1_K2.df)
 }
 
-compute.summary.stats_St_K1_K2_P <- function(samples, seasons, max.days){
+compute.summary.stats_St_K1_K2_P <- function(samples, seasons, total.days){
   n.seasons <- length(seasons)
   K1 <- extract.samples("K1", samples)   # just one of these
   K2 <- extract.samples("K2", samples)   # just one of these
   P <- extract.samples("P", samples)   # just one of these
   X.low <- X.high <- X.med <- matrix(data = NA, 
                                      nrow = n.seasons, 
-                                     ncol = max(max.days))
-  i <- j <- 1
+                                     ncol = max(total.days))
+  X.list <- vector(mode = "list", length = max(total.days) * n.seasons)
+  X.season <- vector(mode = "list", length = n.seasons)
+  
+  c <- i <- j <- 1
   for (i in 1:n.seasons){   # year
     max_i <- extract.samples(paste0("max[", i, "]"), samples)
     S <- extract.samples(paste0("S[", i, "]"), samples)
-    for (j in 1:max.days[i]){   # days
+    c.start <- c
+    
+    for (j in 1:total.days[i]){   # days
       # state
       M1 <- (1 + (2 * exp(K1) - 1) * exp((1/(-S)) * (P - j))) ^ (-1/exp(K1))
       M2 <- (1 + (2 * exp(K2) - 1) * exp((1/S) * (P - j))) ^ (-1/exp(K2))
-      X <-  max_i * (M1 * M2)
+      #X <-  max_i * (M1 * M2)
+      X.list[[c]] <-  max_i * (M1 * M2)
       
-      X.low[i,j] <- quantile(X, 0.025)
-      X.med[i,j] <- quantile(X, 0.50)      
-      X.high[i,j] <- quantile(X, 0.975)
+      X.low[i,j] <- quantile(X.list[[c]], 0.025)
+      X.med[i,j] <- quantile(X.list[[c]], 0.50)      
+      X.high[i,j] <- quantile(X.list[[c]], 0.975)
+      c <- c + 1
       
     }
+    c.end <- c - 1
+    X.season[[i]] <- list2DF(X.list[c.start:c.end])
   }  
   
   X.low.df <- data.frame(t(X.low)) %>% melt() %>% 
@@ -246,16 +255,23 @@ compute.summary.stats_St_K1_K2_P <- function(samples, seasons, max.days){
   X.high.df <- data.frame(t(X.high)) %>% melt() %>%
     transmute(High = value) 
   
+  X.season.sum <- lapply(X.season, rowSums)
+  
   X_St_K1_K2_P.df <- data.frame(DOS = rep(seq(1, ncol(X.low)), 
-                                        length.out = n.seasons * ncol(X.low)),
-                              Season = rep(seasons,
-                                           each = ncol(X.low)),
-                              Low = X.low.df$Low,
-                              Med = X.med.df$Med,
-                              High = X.high.df$High)  
-  return(X_St_K1_K2_P.df)
+                                          length.out = n.seasons * ncol(X.low)),
+                                Season = rep(seasons,
+                                             each = ncol(X.low)),
+                                Low = X.low.df$Low,
+                                Med = X.med.df$Med,
+                                High = X.high.df$High)  
+  
+  X.out <- list(X.raw = X.list,
+                X.season = X.season,
+                X.season.sums = X.season.sum,
+                X.df = X_St_K1_K2_P.df)
+  
+  return(X.out)
 }
-
 
 compute.summary.stats_S_K1_K2 <- function(samples, seasons, max.days, P){
   
